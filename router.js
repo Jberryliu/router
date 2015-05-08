4ed85028574f5d47aafc32014e06a61dd5879fa7
Jberryliu <liuyz0602@163.com>
2015-05-08 11:39:20 +0800

    init project

---
+++ router.js
@@ -0,0 +1,30 @@
+/**
+ * @author: berry
+ * @time: 2015-05-08
+ * @desc: 动态路由
+ * @todo:
+ */
+
+(function(window){
+
+    /**
+     * @constructor
+     */
+    function Router(){
+
+    }
+
+    Router.prototype = {
+        constructor: Router
+
+    };
+
+    /**
+     * @desc 工具类
+     */
+    var Util = {
+
+    };
+
+    window.router = new Router();
+})(window);
